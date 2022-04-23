12455f02be669e03d8d0016f6bcb4c1076ed00a3
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2022-04-23 09:53:07 -0400

    Adding basic logging and an exception handler for logging

--- stations_api/api.py
+++ stations_api/api.py
@@ -1,7 +1,8 @@
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from stations_api.data.stations_repository import StationsRepository
 from stations_api.models.station import Station
+from loguru import logger
 
 
 class Api:
@@ -11,20 +12,25 @@ class Api:
     def create_app(self):
         app = FastAPI(docs_url="/api/stations/docs", redoc_url="/api/stations/redoc")
 
+        @app.exception_handler(Exception)
+        async def handle_exception(request: Request, exc: Exception):
+            logger.exception(f"Exception occurred for request: {request.path_params}")
+            return {"error": str(exc)}, 500
+
         @app.get("/api/stations/status")
         async def status():
             return "Running!"
 
         @app.get("/api/stations/id/{id}", response_model=Station)
         async def get_station_by_id(id: int):
-            print(f"Retrieving station with id: {id}")
+            logger.info(f"Retrieving station with id: {id}")
             response = await self._stations_repository.get_by_id(id)
-            print(f"Received response: {response}")
+            logger.info(f"Received response: {response}")
             return response
 
         @app.get("/api/stations/name/{name}", response_model=List[Station])
         async def get_stations_by_name(name: str):
-            print(f"Received request for stations with name: {name}")
+            logger.info(f"Received request for stations with name: {name}")
             return await self._stations_repository.get_by_name(name)
 
         return app
